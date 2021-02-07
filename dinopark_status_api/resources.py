91d35277eba5050ea1e1f1438acb8e04eb5a8fc5
Ju Young Kim <juyoung.kim517@gmail.com>
2021-02-07 19:07:41 +0200

    add business logic for maintenance

--- dinopark_status_api/resources.py
+++ dinopark_status_api/resources.py
@@ -6,6 +6,8 @@ API resource mapped to REST routes.
 # System imports
 import logging
 import requests
+import time
+from datetime import datetime
 
 # Third-party imports
 from flask import make_response, jsonify
@@ -36,6 +38,10 @@ class Health(Resource):
 class StatusMaintenance(Resource):
     """
     End-point for providing the zone maintenance status in Dino Park for a given zone identifier.
+
+    The NUDLS endpoint returns maintenance performed date. This endpoint will calculate the difference between
+    today's date (when the API was called) and the retrieved maintenance date to decide whether maintenance is required or not.
+
     """
 
     def __init__(self, **kwargs):
@@ -70,19 +76,44 @@ class StatusMaintenance(Resource):
         # Retrieve content of the logs
         content = resp.json()
 
-        # Check if maintenance is required. If the given zone is not in the logs, an error is returned.
+        # Retrieve maintenance performed date
+        maintenance_log = [entry for entry in content if entry["kind"] == "maintenance_performed"]
+        filter_by_zone = [entry for entry in maintenance_log if entry["location"] == zone][0]
+        maintenance_date = filter_by_zone["time"]
 
+        # Retrieve today's date and maintenance date
+        today = time.strftime("%Y-%m-%d")
+        maintenance_date = datetime.strptime(maintenance_date, "%Y-%m-%dT%H:%M:%S.%fZ").strftime("%Y-%m-%d")
 
-        # Insert status result into MongoDB and return insert count
-        insert_docs = self._collection.insert_many(content)
-        insert_count = len(insert_docs.inserted_ids)
-        self._logger.error(f"Number of documents inserted: {insert_count}")
+        # Convert to datetime object
+        date_obj_1 = datetime.strptime(today, "%Y-%m-%d")
+        date_obj_2 = datetime.strptime(maintenance_date, "%Y-%m-%d")
+
+        # Calculate the difference in days
+        date_diff = (date_obj_1 - date_obj_2).days
+
+        # Decide whether maintenance is required or not
+        if date_diff < 30:
+            maintenance_status = f"Maintenance is not required. Currently {date_diff} days after last maintenance performed."
+            maintenance_required = 0
+        elif date_diff == 30:
+            maintenance_status = f"Maintenance is not required, but maintenance will be required from tomorrow."
+            maintenance_required = 0
+        else:
+            maintenance_status = f"Maintenance is required. Currently {date_diff} days after last maintenance performed."
+            maintenance_required = 1
 
         # Final response body of the API - zone will be a partition key inside document DB
         result = {
             "zone": zone,
-            "maintenance": "required",
+            "maintenance_required": maintenance_required,
+            "info": maintenance_status
         }
+
+        # Insert status result into MongoDB and return insert count
+        insert_docs = self._collection.insert_many(result)
+        insert_count = len(insert_docs.inserted_ids)
+        self._logger.error(f"Number of documents inserted: {insert_count}")
 
         self._logger.error(f"Processed maintenance status request for zone: {zone}")
 
